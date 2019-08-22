3f829fcf436ab3d88aa763ea49ef440c0107a5a4
dexterchan <dexterchan@gmail.com>
2019-08-22 23:12:17 +0800

    fix

--- pkg/service/risk/risk-service.go
+++ pkg/service/risk/risk-service.go
@@ -45,20 +45,20 @@ func (fake FakeRiskCalcHelper) Calculate(c context.Context, req *risk.ValueReque
 	//Fill in asset
 	for asset := 0; asset < numOfAsset; asset++ {
 		assetBuilder := risk.ValueResponse_AssetSensivity{
-			AssetId:  "asset" + string(asset),
+			AssetId:  fmt.Sprintf("asset%d", asset),
 			Ccy:      "USD",
 			Npv:      math.Pow(-1.0, float64(asset)) * 1000.0,
 			SenseLst: make([]*risk.ValueResponse_Sensitivity, 0),
 		}
 		for i := 0; i < numOfRiskSense; i++ {
 			sens := risk.ValueResponse_Sensitivity{
-				RiskLabel: "risk" + string(0),
+				RiskLabel: fmt.Sprintf("risk%d", i),
 				Ccy:       "USD",
 				Tenors:    make([]*risk.ValueResponse_Sensitivity_Tenor, 0),
 			}
 			for j := 0; j < fakeTenor; j++ {
 				tenor := risk.ValueResponse_Sensitivity_Tenor{
-					Label: "tenor" + string(j),
+					Label: fmt.Sprintf("tenor%d", j),
 					Value: math.Pow(-1.0, float64(asset)) * 1000.0 * float64(j+1),
 				}
 				sens.Tenors = append(sens.Tenors, &tenor)
